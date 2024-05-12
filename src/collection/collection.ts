6e055175b7eac2f78f3c170e49dc479006e5696f
wmxpy <wm@mengw.io>
2024-05-11 19:53:58 -0500

    chore: Update createPage, putPage, retitlePage, deletePage, hasPage, getPage, listPages, listDirectories, searchPages, and queryPages signatures in MongoImbricateCollection

--- src/collection/collection.ts
+++ src/collection/collection.ts
@@ -4,89 +4,108 @@
  * @description Collection
  */
 
-import { IImbricateOriginCollection, IImbricatePage, ImbricateCollectionCapability, ImbricatePageMetadata, ImbricatePageQuery, ImbricatePageQueryConfig, ImbricatePageSearchResult, ImbricatePageSnapshot, ImbricateSearchPageConfig, PromiseOr } from "@imbricate/core";
+import { IImbricateOriginCollection, IImbricatePage, ImbricateCollectionCapability, ImbricatePageMetadata, ImbricatePageQuery, ImbricatePageQueryConfig, ImbricatePageSearchResult, ImbricatePageSnapshot, ImbricateSearchPageConfig, createAllAllowImbricateCollectionCapability } from "@imbricate/core";
+import { ICollectionModel } from "../database/collection/model";
 
 export class MongoImbricateCollection implements IImbricateOriginCollection {
 
-    collectionName: string;
-    uniqueIdentifier: string;
+    public static withModel(collection: ICollectionModel): MongoImbricateCollection {
 
-    description?: string | undefined;
-    capabilities: ImbricateCollectionCapability;
+        return new MongoImbricateCollection(collection);
+    }
+
+    private readonly _collection: ICollectionModel;
+
+    public readonly capabilities: ImbricateCollectionCapability = createAllAllowImbricateCollectionCapability();
+
+    private constructor(collection: ICollectionModel) {
+
+        this._collection = collection;
+    }
+
+    public get collectionName(): string {
+        return this._collection.collectionName;
+    }
+    public get uniqueIdentifier(): string {
+        return this._collection.uniqueIdentifier;
+    }
+    public get description(): string | undefined {
+        return this._collection.description;
+    }
 
     createPage(
-        directories: string[],
-        title: string,
-        initialContent: string,
+        _directories: string[],
+        _title: string,
+        _initialContent: string,
     ): Promise<IImbricatePage> {
 
         throw new Error("Method not implemented.");
     }
 
     putPage(
-        pageMetadata: ImbricatePageMetadata,
-        content: string,
+        _pageMetadata: ImbricatePageMetadata,
+        _content: string,
     ): Promise<IImbricatePage> {
 
         throw new Error("Method not implemented.");
     }
 
     retitlePage(
-        identifier: string,
-        newTitle: string,
+        _identifier: string,
+        _newTitle: string,
     ): Promise<void> {
 
         throw new Error("Method not implemented.");
     }
 
     deletePage(
-        identifier: string,
+        _identifier: string,
     ): Promise<void> {
 
         throw new Error("Method not implemented.");
     }
 
     hasPage(
-        directories: string[],
-        title: string,
+        _directories: string[],
+        _title: string,
     ): Promise<boolean> {
 
         throw new Error("Method not implemented.");
     }
 
     getPage(
-        identifier: string,
+        _identifier: string,
     ): Promise<IImbricatePage | null> {
 
         throw new Error("Method not implemented.");
     }
 
     listPages(
-        directories: string[],
-        recursive: boolean,
+        _directories: string[],
+        _recursive: boolean,
     ): Promise<ImbricatePageSnapshot[]> {
 
         throw new Error("Method not implemented.");
     }
 
     listDirectories(
-        directories: string[],
+        _directories: string[],
     ): Promise<string[]> {
 
         throw new Error("Method not implemented.");
     }
 
     searchPages(
-        keyword: string,
-        config: ImbricateSearchPageConfig,
+        _keyword: string,
+        _config: ImbricateSearchPageConfig,
     ): Promise<ImbricatePageSearchResult[]> {
 
         throw new Error("Method not implemented.");
     }
 
     queryPages(
-        query: ImbricatePageQuery,
-        config: ImbricatePageQueryConfig,
+        _query: ImbricatePageQuery,
+        _config: ImbricatePageQueryConfig,
     ): Promise<IImbricatePage[]> {
 
         throw new Error("Method not implemented.");
