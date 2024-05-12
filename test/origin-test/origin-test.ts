fc9de3818df0dd7f280fb580a6cb3d1ae28b2614
wmxpy <wm@mengw.io>
2024-05-11 22:34:15 -0500

    feat: Refactor Imbricate Origin tests and update dependencies

--- test/origin-test/origin-test.ts
+++ test/origin-test/origin-test.ts
@@ -7,16 +7,25 @@
 import { IImbricateOrigin } from "@imbricate/core";
 import { startImbricateOriginCollectionCreateTest } from "./collection/create";
 import { disposeOrigin } from "./common";
+import { ImbricateOriginTestConstructionFunction } from "./testing-target";
 
 export const startImbricateOriginTest = (
-    origin: IImbricateOrigin,
+    constructFunction: ImbricateOriginTestConstructionFunction,
 ): void => {
 
-    afterAll(async () => {
-        await disposeOrigin(origin);
-    });
+    describe("Test Imbricate Origin with Imbricate Origin Testing Jest", () => {
 
-    startImbricateOriginCollectionCreateTest(
-        origin,
-    );
+        beforeAll(async () => {
+
+            origin = await constructFunction();
+        });
+
+        afterAll(async () => {
+            await disposeOrigin(origin);
+        });
+
+        startImbricateOriginCollectionCreateTest(
+            origin,
+        );
+    });
 };
---
+++ test/origin-test/testing-target.ts
@@ -0,0 +1,9 @@
+/**
+ * @author WMXPY
+ * @namespace OriginTest
+ * @description Definition
+ */
+
+import { IImbricateOrigin } from "@imbricate/core";
+
+export type ImbricateOriginTestConstructionFunction = () => Promise<IImbricateOrigin>;
