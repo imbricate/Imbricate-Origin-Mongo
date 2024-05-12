1582679aab8b2c48677ef4cc0bd7eb9a0843f61d
wmxpy <wm@mengw.io>
2024-05-11 19:50:50 -0500

    chore: Update createPage, putPage, retitlePage, deletePage, hasPage, getPage, listPages, listDirectories, searchPages, and queryPages signatures in MongoImbricateCollection

--- src/collection/collection.ts
+++ src/collection/collection.ts
@@ -10,37 +10,85 @@ export class MongoImbricateCollection implements IImbricateOriginCollection {
 
     collectionName: string;
     uniqueIdentifier: string;
+
     description?: string | undefined;
     capabilities: ImbricateCollectionCapability;
-    createPage(directories: string[], title: string, initialContent: string): PromiseOr<IImbricatePage> {
-        throw new Error("Method not implemented.");
-    }
-    putPage(pageMetadata: ImbricatePageMetadata, content: string): PromiseOr<IImbricatePage> {
-        throw new Error("Method not implemented.");
-    }
-    retitlePage(identifier: string, newTitle: string): PromiseOr<void> {
-        throw new Error("Method not implemented.");
-    }
-    deletePage(identifier: string): PromiseOr<void> {
-        throw new Error("Method not implemented.");
-    }
-    hasPage(directories: string[], title: string): PromiseOr<boolean> {
-        throw new Error("Method not implemented.");
-    }
-    getPage(identifier: string): PromiseOr<IImbricatePage | null> {
-        throw new Error("Method not implemented.");
-    }
-    listPages(directories: string[], recursive: boolean): PromiseOr<ImbricatePageSnapshot[]> {
-        throw new Error("Method not implemented.");
-    }
-    listDirectories(directories: string[]): PromiseOr<string[]> {
-        throw new Error("Method not implemented.");
-    }
-    searchPages(keyword: string, config: ImbricateSearchPageConfig): PromiseOr<ImbricatePageSearchResult[]> {
-        throw new Error("Method not implemented.");
-    }
-    queryPages(query: ImbricatePageQuery, config: ImbricatePageQueryConfig): PromiseOr<IImbricatePage[]> {
+
+    createPage(
+        directories: string[],
+        title: string,
+        initialContent: string,
+    ): Promise<IImbricatePage> {
+
         throw new Error("Method not implemented.");
     }
 
+    putPage(
+        pageMetadata: ImbricatePageMetadata,
+        content: string,
+    ): Promise<IImbricatePage> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    retitlePage(
+        identifier: string,
+        newTitle: string,
+    ): Promise<void> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    deletePage(
+        identifier: string,
+    ): Promise<void> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    hasPage(
+        directories: string[],
+        title: string,
+    ): Promise<boolean> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    getPage(
+        identifier: string,
+    ): Promise<IImbricatePage | null> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    listPages(
+        directories: string[],
+        recursive: boolean,
+    ): Promise<ImbricatePageSnapshot[]> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    listDirectories(
+        directories: string[],
+    ): Promise<string[]> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    searchPages(
+        keyword: string,
+        config: ImbricateSearchPageConfig,
+    ): Promise<ImbricatePageSearchResult[]> {
+
+        throw new Error("Method not implemented.");
+    }
+
+    queryPages(
+        query: ImbricatePageQuery,
+        config: ImbricatePageQueryConfig,
+    ): Promise<IImbricatePage[]> {
+
+        throw new Error("Method not implemented.");
+    }
 }
